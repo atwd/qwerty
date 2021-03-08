ae50ffb1cc920368a675439ec1bf6e4ac7a823e4
unknown <bsana7931@gmail.com>
2021-03-08 10:23:29 +0300

    eeu

--- src/travel/settings/production.py
+++ src/travel/settings/production.py
@@ -32,7 +32,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['find-route-test.herokuapp.com',]
+ALLOWED_HOSTS = ['find-route-test.herokuapp.com']
 
 
 # Application definition
