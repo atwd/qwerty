a33ea91ebb426bd6a43ac6ea6e3568ae51450aea
unknown <bsana7931@gmail.com>
2021-03-08 10:06:53 +0300

    qw

--- src/travel/settings/production.py
+++ src/travel/settings/production.py
@@ -17,7 +17,7 @@ DB_NAME = os.environ.get('DB_NAME')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
 DB_HOST = os.environ.get('DB_HOST')
 DB_USER = os.environ.get('DB_USER')
-#SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -27,12 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't3d$4wul8d)*c6p_qxo#g*d5-)3)3+4)@f4_*yigh6b&oplho='
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['find-route-test.herokuapp.com',]
 
 
 # Application definition
@@ -97,6 +97,7 @@ DATABASES = {
 }
 
 import dj_database_url
+
 db = dj_database_url.config()
 DATABASES['default'].update(db)
 
